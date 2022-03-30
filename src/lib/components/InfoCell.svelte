1f8fbadb5f0d1caad0a7ac9a5fd205d4b31f8fbf
Helio Chissini de Castro <helio@kde.org>
2022-03-30 23:22:17 +0200

    fix(layout): Improve InfoCell component
    Signed-off-by: Helio Chissini de Castro <helio@kde.org>

--- src/lib/components/InfoCell.svelte
+++ src/lib/components/InfoCell.svelte
@@ -5,7 +5,7 @@ This program and the accompanying materials are made
 available under the terms of the Eclipse Public License 2.0
 which is available at https://www.eclipse.org/legal/epl-2.0/ -->
 <div class="px-3 py-3 border border-gray-200 w-full">
-	<div class="grid grid-cols-3">
+	<div class="grid grid-cols-2">
 		<slot />
 	</div>
 </div>
--- src/routes/licenses/[slug].svelte
+++ src/routes/licenses/[slug].svelte
@@ -14,28 +14,33 @@ which is available at https://www.eclipse.org/legal/epl-2.0/ -->
 	import checkCircleSrc from '$lib/icons/check-circle.svg?src';
 	import timesCircleSrc from '$lib/icons/times-circle.svg?src';
 
-	function nl2br(str) {
-		let data = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
-		return data;
-	}
-
 	export let license;
 
 	let options = 'Details';
 	let name = license.shortName ? license.shortName : 'License';
 	let parent = 'Licenses';
+
+	function nl2br(str) {
+		let data = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+		return data;
+	}
+
+	function checkIcon(value) {
+		let res = value
+			? `<div class="w-4 fill-green-700">${checkCircleSrc}</div>`
+			: `<div class="w-4 fill-red-700">${timesCircleSrc}</div>`;
+		return res;
+	}
 </script>
 
 <PageHeader {name} {parent} />
 
 <div class="sw360-gridpanel">
 	<div>
-		<div>
-			<div class="grid grid-cols-1 bg-white border border-gray-200 rounded w-full text-gray-500">
-				<RadioButton value="Details" bind:options>Details</RadioButton>
-				<RadioButton value="Text" bind:options>Text</RadioButton>
-				<RadioButton value="Obligations" bind:options>Obligations</RadioButton>
-			</div>
+		<div class="grid grid-cols-1 bg-white border border-gray-200 rounded w-full text-gray-500">
+			<RadioButton value="Details" bind:options>Details</RadioButton>
+			<RadioButton value="Text" bind:options>Text</RadioButton>
+			<RadioButton value="Obligations" bind:options>Obligations</RadioButton>
 		</div>
 	</div>
 
@@ -44,64 +49,55 @@ which is available at https://www.eclipse.org/legal/epl-2.0/ -->
 			<button class="sw360-button">Edit License</button>
 		</ComponentHeader>
 
-		<div class="col-span-2 bg-white w-full text-gray-900">
+		<div class="col-span-2 grid grid-cols-1">
 			{#if options == 'Details'}
 				<div class="sw360-navy-bg-text">License Details</div>
 				<InfoCell>
-					<span>Fullname:</span>
-					<div class="cols-span-2">{license.fullName}</div>
-				</InfoCell>
-				<InfoCell>
-					<span>Shortname:</span>
-					<div class="cols-span-2">{license.shortName}</div>
+					<div>Fullname:</div>
+					<div>{license.fullName}</div>
 				</InfoCell>
 
 				<InfoCell>
-					<span>Is Checked:</span>
+					<div>Shortname:</div>
+					<div>{license.shortName}</div>
+				</InfoCell>
 
-					<div class="grid grid-cols-1">
-						{#if license.checked}
-							{@html `<div class="w-4 fill-green-700">${checkCircleSrc}</div>`}
-						{:else}
-							{@html `<div class="w-4 fill-red-700">${timesCircleSrc}</div>`}
-						{/if}
-					</div>
-				</InfoCell>
 				<InfoCell>
-					<div class="grid grid-cols-3">
-						<span>Type:</span>
+					<div>Is Checked:</div>
+					{@html checkIcon(license.checked)}
+				</InfoCell>
+
+				<InfoCell>
+					<div>Type:</div>
+					<div />
+				</InfoCell>
+
+				<InfoCell>
+					<div>OSI Approved?:</div>
+					<div>
+						{@html checkIcon(license.OSIApproved !== 'NA')}
 					</div>
 				</InfoCell>
-				<div class="px-3 py-3 border border-gray-200 w-full">
-					<div class="grid grid-cols-3">
-						<span>OSI Approved?:</span>
-						{#if license.OSIApproved == 'NA'}
-							{@html `<div class="w-4 fill-red-700">${timesCircleSrc}</div>`}
-						{:else}
-							{@html `<div class="w-4 fill-green-700">${checkCircleSrc}</div>`}
-						{/if}
+
+				<InfoCell>
+					<div>FSF Free/Libre?:</div>
+					{@html checkIcon(license.OSIApproved !== 'NA')}
+				</InfoCell>
+
+				<InfoCell>
+					<div class="place-self-center justify-self-start">
+						External link for more information:
 					</div>
-				</div>
-				<div class="px-3 py-3 border border-gray-200 w-full">
-					<div class="grid grid-cols-3">
-						<span>FSF Free/Libre?:</span>
-						{#if license.OSIApproved == 'NA'}
-							{@html `<div class="w-4 fill-red-700">${timesCircleSrc}</div>`}
-						{:else}
-							{@html `<div class="w-4 fill-green-700">${checkCircleSrc}</div>`}
-						{/if}
+					<div class="flex gap-3 items-center">
+						<input type="text" class="form-control px-2 h-10 border rounded" />
+						<button class="sw360-button-reverse">Save</button>
 					</div>
-				</div>
-				<div class="px-3 py-3 border border-gray-200 w-full">
-					<div class="grid grid-cols-3">
-						<span>External link for more information:</span>
-					</div>
-				</div>
+				</InfoCell>
 			{:else if options == 'Text'}
-				<div class="text-left px-3 py-4 w-full bg-sw360-navyblue text-white font-bold break-normal">
-					License Text
+				<div class="col-span-2 sw360-navy-bg-text">License Text</div>
+				<div class="col-span-2 px-3 py-3 border border-gray-200 w-full">
+					{@html nl2br(license.text)}
 				</div>
-				<div class="px-3 py-3 border border-gray-200 w-full">{@html nl2br(license.text)}</div>
 			{/if}
 		</div>
 	</div>
