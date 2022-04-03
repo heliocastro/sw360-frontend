93aba82bb27e3253f73543437fb3444cc31d54a5
Helio Chissini de Castro <helio@kde.org>
2022-04-03 18:55:12 +0200

    ifeature(panel): Add ECC boilerplate
    ECC releases has not direct api to execute the process.
    For a possible solution is list all projects, then list all releases
    from there then get the ECC value
    
    Signed-off-by: Helio Chissini de Castro <helio@kde.org>

--- src/routes/ecc/index.svelte
+++ src/routes/ecc/index.svelte
@@ -7,25 +7,92 @@ which is available at https://www.eclipse.org/legal/epl-2.0/
 
 SPDX-License-Identifier: EPL-2.0 -->
 <script context="module" lang="ts">
-	export const prerender = true;
+  export const prerender = true
 
-	export async function load({ session }) {
-		if (!session.user) {
-			return {
-				status: 302,
-				redirect: '/login'
-			};
-		}
+  export async function load({ fetch, session }) {
+    if (!session.user) {
+      return {
+        status: 302,
+        redirect: '/login'
+      }
+    }
 
-		return {
-			props: {}
-		};
-	}
+    return {
+      props: {}
+    }
+  }
 </script>
 
 <script lang="ts">
-	import PageHeader from '$lib/components/PageHeader.svelte';
-	let name = 'ECC';
+  import ComponentHeader from '$lib/components/ComponentHeader.svelte'
+  import PageHeader from '$lib/components/PageHeader.svelte'
+  import QuickFilter from '$lib/components/QuickFilter.svelte'
+  import Grid from 'gridjs-svelte'
+  import { html } from 'gridjs'
+  import { SvelteWrapper } from 'gridjs-svelte/plugins'
+
+  const data = []
+  let grid
+  let name = 'ECC'
+
+  let pagination = {
+    enabled: true,
+    limit: 10
+  }
+  let search = {
+    keyword: ''
+  }
+
+  const columns = [
+    { name: 'Status', width: '10%', formatter: cell => html(cell) },
+    {
+      name: 'Release name',
+      formatter: cell => html(cell)
+    },
+    {
+      name: 'Release version'
+    },
+    {
+      name: 'Creator Group'
+    },
+    {
+      name: 'ECC Assessor'
+    },
+    {
+      name: 'ECC Assessor Group'
+    },
+    {
+      name: 'ECC Assessment Date'
+    }
+  ]
+
+  async function doSearch() {
+    search.keyword = this.value
+    grid.updateConfig({ search }).forceRender()
+  }
+
+  async function doLimit() {
+    pagination.limit = this.value
+    grid.updateConfig({ pagination }).forceRender()
+  }
 </script>
 
 <PageHeader {name} />
+
+<div class="sw360-gridpanel">
+  <QuickFilter searchFunction={doSearch} />
+
+  <div class="sw360-gridpanel-content-r">
+    <ComponentHeader name="ECC OVERVIEW" />
+    <div class="col-span-2">
+      <div class="my-4 text-base text-gray-600">
+        Show <span>
+          <select on:change={doLimit} class="bg-white rounded border p-1">
+            <option selected>10</option><option>25</option><option>50</option><option>100</option>
+          </select>
+        </span>entries
+      </div>
+      <Grid bind:instance={grid} {data} {columns} sort {search} {pagination} />
+    </div>
+  </div>
+</div>
